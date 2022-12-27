17caf4149fbd9eaa6090d04cabfc957c846a914c
Maximiliano Cruz <maxi7587@hotmail.com>
2022-12-27 13:34:48 -0300

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthService } from "./shared/auth.service";
+
+class MockAuthService {
+  doLogout = () => {}
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,6 +16,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService }
+      ]
     }).compileComponents();
   });
 
@@ -20,16 +28,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-jwt'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-jwt');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-jwt app is running!');
+    expect(compiled.querySelector('h5')?.textContent).toContain('Angular Auth');
+  });
+
+  it('should render SignIn', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const links = compiled.querySelectorAll('a');
+    expect(links.item(0).textContent).toContain('Sign in')
+  });
+
+  it('' +
+    'should logout', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const componentInstance = fixture.componentInstance;
+    const logoutSpy = spyOn(componentInstance.authService, 'doLogout');
+    fixture.componentInstance.logout();
+    expect(logoutSpy).toHaveBeenCalled();
   });
 });
